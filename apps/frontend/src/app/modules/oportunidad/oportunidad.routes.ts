ba38772e473b958d847ac5fefca3ea5369c1fe11
FrederickMontiel <86737117+FrederickMontiel@users.noreply.github.com>
2026-06-03 20:38:14 -0600

    fix: Reactivar ruta actividades y reordenar para evitar conflictos

--- apps/frontend/src/app/modules/oportunidad/oportunidad.routes.ts
+++ apps/frontend/src/app/modules/oportunidad/oportunidad.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { OportunidadListComponent } from './oportunidad-list/oportunidad-list.component';
 import { OportunidadDetailComponent } from './oportunidad-detail/oportunidad-detail.component';
+import { OportunidadActividadesComponent } from './oportunidad-actividades/oportunidad-actividades.component';
 
 export const oportunidadRoutes: Routes = [
   { path: '', component: OportunidadListComponent },
   { path: 'nuevo', component: OportunidadDetailComponent },
+  { path: ':id/actividades', component: OportunidadActividadesComponent },
   { path: ':id', component: OportunidadDetailComponent },
   { path: ':id/editar', component: OportunidadDetailComponent },
 ];
